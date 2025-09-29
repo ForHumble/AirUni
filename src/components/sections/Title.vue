b8a7e22eb3bfcd643579a0ff16730a949d45343f
Yuzhao Luo <136302391+ForHumble@users.noreply.github.com>
2025-09-29 19:28:22 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -4,16 +4,16 @@ import { ElIcon } from 'element-plus'
 import { Document, Files, MagicStick, Picture, DataAnalysis, Film } from '@element-plus/icons-vue'
 
 // logo地址，没有则置为""即可
-const logo = './logo.png'
+const logo = ''
 
 // 标题
-const title = 'Academic Project Page Template'
+const title = 'AirUni'
 
 // 标题颜色
 const title_color = '#000000'
 
 // 标题补充，没有则置为''即可
-const title_supp = ' (Vue based)'
+const title_supp = ' '
 
 // 标题补充颜色
 const title_supp_color = '#42B883'
@@ -24,54 +24,52 @@ const btn_color = '#444444'
 // 作者清单（包含作者姓名、头像、主页、地址序号）
 const authors = [
   {
-    name: "Your Name",
-    icon: "./icon/junyaohu.jpg",
-    homepage: "https://junyaohu.github.io/",
+    name: "Yuzhao Luo",
     address_flag: "1,#"
   },
-  {
-    name: "Anya Forger",
-    icon: "./icon/anya.jpg",
-    homepage: "https://www.bilibili.com/video/BV1jv4y1P7Bb",
-    address_flag: "2,#"
-  },
-  {
-    name: "BugCat Capoo",
-    icon: "./icon/capoo.webp",
-    homepage: "https://zh.moegirl.org.cn/%E7%8C%AB%E7%8C%AB%E8%99%AB%E5%92%96%E6%B3%A2",
-    address_flag: "1,*"
-  },
+  // {
+  //   name: "Anya Forger",
+  //   icon: "./icon/anya.jpg",
+  //   homepage: "https://www.bilibili.com/video/BV1jv4y1P7Bb",
+  //   address_flag: "2,#"
+  // },
+  // {
+  //   name: "BugCat Capoo",
+  //   icon: "./icon/capoo.webp",
+  //   homepage: "https://zh.moegirl.org.cn/%E7%8C%AB%E7%8C%AB%E8%99%AB%E5%92%96%E6%B3%A2",
+  //   address_flag: "1,*"
+  // },
 ]
 
 // 地址清单（包含地址名称、头像、主页、地址序号）
 const addresses = [
   {
     address_flag: "1",
-    name: "Home University",
-    icon: "./icon/home.png",
-    homepage: "https://github.com/hmuniversity"
+    name: "Beihang University",
   },
-  {
-    address_flag: "2",
-    name: "IKUN University",
-    icon: "./icon/ikun.avif",
-    homepage: "https://www.bilibili.com/video/BV178411Y7QB"
-  },
+  // {
+  //   address_flag: "2",
+  //   name: "IKUN University",
+  //   icon: "./icon/ikun.avif",
+  //   homepage: "https://www.bilibili.com/video/BV178411Y7QB"
+  // },
 ]
 
 // 共一和通讯提示
 const con_and_corresponding_author = 
   "#: Equal Contribution. *: Corresponding Author."
 
-// 最新消息
-const news = "🔥 [2024-12-15] This template project is still under development."
+// // 最新消息
+// const news = "🔥 [2024-12-15] This template project is still under development."
 
 // 强调内容
-const emphases = [
-  "🎉 [ABCD 2024] Poster",
-  "🥰 欢迎关注“减论”微信公众号/B站/知乎/小红书",
-  "传递人工智能算法科普教育的减约理解",
-  "提升信息效率及认知维度"
+// const emphases = [
+//   "🎉 [ABCD 2024] Poster",
+//   "🥰 欢迎关注“减论”微信公众号/B站/知乎/小红书",
+//   "传递人工智能算法科普教育的减约理解",
+//   "提升信息效率及认知维度"
+// ]
+	const emphases = [
 ]
 
 // 提供引导资料链接
@@ -80,45 +78,25 @@ const buttons = [
     disabled: true,
     name: "Paper",
     component: Document,
-  },
-  {
-    disabled: true,
-    name: "中译版",
-    component: Document,
+	  // link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
   },
   {
     disabled: false,
     name: "Code",
-    link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
+    // link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
     component: Files,
   },
   {
-    disabled: false,
-    name: "Demo",
-    link: "https://junyaohu.github.io/academic-project-page-template-vue",
-    component: MagicStick,
-  },
-  {
     disabled: true,
-    name: "Poster",
+    name: "DataSet(Huggingface)",
+    // link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
     component: Picture,
   },
   {
     disabled: true,
-    name: "Slide",
-    component: DataAnalysis,
-  },
-  {
-    disabled: false,
-    name: "Video (减论)",
-    link: "https://www.bilibili.com/video/BV15XkgYiE73/",
-    component: Film,
-  },
-  {
-    disabled: false,
-    name: "Video (Tutorial)",
-    link: "https://www.bilibili.com/video/BV1oUrfYzEqZ",
-    component: Film,
+    name: "DataSet(ModelScope)",
+    // link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
+    component: Picture,
   },
 ]
 
