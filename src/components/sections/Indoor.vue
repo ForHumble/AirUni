7d422e983e4396ad50e7a18a9c6481c03348bcc6
Yuzhao Luo <136302391+ForHumble@users.noreply.github.com>
2025-10-09 21:04:15 +0800

    Create Indoor.vue

---
+++ src/components/sections/Indoor.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+      <h1 class="section-title">Indoor Successful Examples</h1>
+    </el-row>
+
+    <!-- 每个网站的视频的iframe可能不一致，最好在这里手动调整 -->
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
+
+        <!-- local -->
+        <el-container class="video-container">
+          <video controls muted preload playsinline>
+            <source src="/video/Indoor/room2room2table_speed.mp4" type="video/mp4">
+          </video>
+        </el-container>
+        
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+
+.video-container{
+  margin: 20px 0px 0px 0px;
+}
+
+iframe, video {
+  aspect-ratio: 16 / 9;
+  width: 100%;
+}
+
+</style>
