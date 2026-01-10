75795b1f418274d3b278ee3e81aa690cad921899
Yuzhao Luo <136302391+ForHumble@users.noreply.github.com>
2026-01-10 23:43:08 +0800

    Update Experiment.vue

--- src/components/sections/Experiment.vue
+++ src/components/sections/Experiment.vue
@@ -12,21 +12,9 @@ const experient_implemention = './experient_implemention.png'
       class="caption-container"
     >
       <p>
-        We employed the SU17 UAV developed by AMOVLAB as the embodied platform.
-        The vehicle integrates a gimbal-mounted forward-facing camera (configured at 1280 × 780 resolution) with an omnidirectional vision system, 
-        and is controlled via the PX4 autopilot. A Wi-Fi signal module and a wireless receiver were mounted on top of the UAV to establish communication with the server, 
-        while a grayscale camera was coupled with the SU17 to enable waypoint control through its API. To comprehensively assess its generalization capability, 
-        we evaluate AirUni across a wide spectrum of real-world environments, covering both indoor (e.g., offices, bedrooms) and outdoor (e.g., industrial areas) scenarios.
+       For real-world validation, we employed the SU17 UAV developed by AMOVLAB and the Owl UAV as the embodied platform. Both UAVs integrate a gimbal-mounted forward-facing camera with an omnidirectional vision system, and is controlled via the PX4 autopilot. A Wi-Fi signal module and a wireless receiver were mounted on top of the UAV to establish communication with the server, while a grayscale camera was coupled with the UAV to enable waypoint control through its API.
       </p>
     </el-col>
-    </el-row>
-    <el-row justify="center" style="margin-top: 20px;">
-      <el-col :xs="24" :sm="24" :md="20" :lg="16" :xl="12" >
-        <el-container class="video-container">
-          <!-- <img src="./cover.png" alt="cover" style="width: 100%; height: auto;"> -->
-          <el-image :src="experient_implemention" style="width: 100%; height: auto;" fit="cover" />
-        </el-container>
-      </el-col>
     </el-row>
     
 
