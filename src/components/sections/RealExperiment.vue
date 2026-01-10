6701aecc9f42d5634d67a966d429b46c01bd647d
Yuzhao Luo <136302391+ForHumble@users.noreply.github.com>
2026-01-10 23:17:49 +0800

    Create RealExperiment.vue

---
+++ src/components/sections/RealExperiment.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+      <h1 class="section-title">Explainer Video</h1>
+    </el-row>
+
+    <!-- 每个网站的视频的iframe可能不一致，最好在这里手动调整 -->
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
+
+        <!-- youtube -->
+        <el-container class="video-container">
+          <iframe src="https://youtu.be/Ym9ZeRszzAM" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </el-container>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+
+.video-container{
+  margin: 20px 0px 0px 0px;
+}
+
+iframe, video {
+  aspect-ratio: 16 / 9;
+  width: 100%;
+}
+
+</style>
